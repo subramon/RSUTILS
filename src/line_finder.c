75b84de01968afb3058e2b762625978318512534
Ramesh Subramonian <ramesh.subramonian@target.com>
2024-12-02 21:37:57 +0000

    ok

--- src/line_finder.c
+++ src/line_finder.c
@@ -126,7 +126,9 @@ line_finder(
             }
           }
           if ( null_cell ) { 
-            cells[col_idx][0] = '\0';
+            if ( is_load[col_idx] ) { 
+              cells[col_idx][0] = '\0';
+            }
             col_idx++;
             break;
           }
