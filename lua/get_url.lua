ef08213f9112fb968724fc52fb9470f193b1e01c
Ramesh Subramonian <ramesh.subramonian@target.com>
2025-05-26 05:10:58 +0000

    ok

--- lua/get_url.lua
+++ lua/get_url.lua
@@ -1,32 +1,28 @@
 --  sudo luarocks install Lua-cURL
 local cURL = require("lcurl")
-local cutils = require 'libcutils'
 local function get_url(url, opfile)
   -- open output file
-  f = assert(io.open(opfile, "w"))
   
+  local y = ""
+  local function to_str(x) y = y .. x end 
   cURL.easy()
     -- setup url
     :setopt_url(url)
     -- setup file object as writer
-    :setopt_writefunction(f)
+    :setopt_writefunction(to_str)
     -- perform, invokes callbacks
     :perform()
   -- close easy
   :close()
   
-  -- close output file
-  f:close()
-  return true
+  return y
 end
--- return get_url
+return get_url
 -- quick and dirty test
-local opfile = "_xx.html"
-cutils.unlink(opfile)
-assert(get_url("http://www.example.com", opfile))
-assert(cutils.isfile(opfile))
-cutils.unlink(opfile)
-assert(get_url("https://www.google.com", opfile))
-assert(cutils.isfile(opfile))
-cutils.unlink(opfile)
+--[[
+local x = assert(get_url("http://www.example.com", opfile))
+print(type(x))
+print(#x)
+print(x)
 print("done")
+--]]
